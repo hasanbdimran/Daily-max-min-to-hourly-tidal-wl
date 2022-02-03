21578a842ac8cfe2c537523f10ca6be8d9f6fa85
Mohammad Imran Hasan <hasanbdimran@gmail.com>
2022-02-03 10:10:14 +0600

    Create upsamplingScript.py

---
+++ upsamplingScript.py
@@ -0,0 +1,12 @@
+import fiona
+import shapely
+from shapely.geometry import Point, LineString
+from shapely.ops import unary_union
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_excel(r"D:\CEGIS PROJECT\DWASA_masterplan_meghna\data\Hydrology Wasa\rawdata\WL74.xlsx", sheet_name=1)
+
+
